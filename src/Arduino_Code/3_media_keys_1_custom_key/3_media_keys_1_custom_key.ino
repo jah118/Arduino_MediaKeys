0f43f3b6ad18fa12281f4408aa3f3ea568c74648
Jonas <jonasah97@gmail.com>
2023-12-17 13:45:03 +0100

    Refactor: Changed comment style
    - Updated: pressEventHandler to take KeyboardKeycode to make use of buttonPin4Action1  and buttonPin4Action2
    
    - The code now uses  /// instead of //, for general comments

--- src/Arduino_Code/3_media_keys_1_custom_key/3_media_keys_1_custom_key.ino
+++ src/Arduino_Code/3_media_keys_1_custom_key/3_media_keys_1_custom_key.ino
@@ -6,7 +6,7 @@
   10/12/2023
 
   Dependencies:
-  - HID-Project by NicoHood Version 2.5.0 or above
+  - HID-Project by NicoHood Version 2.8.4 or above
     -- can be found in Arduino Lirary Manager
     -- or: https://github.com/NicoHood/HID
   
@@ -15,16 +15,16 @@
 
   Reference:
   The Reference links below is where the info to getting the project to work comes from
-  https://uboopenfactory.univ-brest.fr/Les-Labs/MusicLab/Projets/Arduino-Media-Keys
+   @see https://uboopenfactory.univ-brest.fr/Les-Labs/MusicLab/Projets/Arduino-Media-Keys
   - most code comes from here.
-  https://github.com/NicoHood/HID/wiki
-  https://www.arduino.cc/reference/en/language/functions/usb/keyboard/
-  https://www.stefanjones.ca/blog/arduino-leonardo-remote-multimedia-keys/
-  https://arduino.stackexchange.com/questions/8934/send-keyboard-media-keys-with-keyboard-library
-  http://www.freebsddiary.org/APC/usb_hid_usages.php
+  @see https://github.com/NicoHood/HID/wiki
+  @see https://www.arduino.cc/reference/en/language/functions/usb/keyboard/
+  @see https://www.stefanjones.ca/blog/arduino-leonardo-remote-multimedia-keys/
+  @see https://arduino.stackexchange.com/questions/8934/send-keyboard-media-keys-with-keyboard-library
+  @see http://www.freebsddiary.org/APC/usb_hid_usages.php
   - list of hid codes for further use or differt use case
-  https://www.instructables.com/id/USB-Volume-Control-and-Caps-Lock-LED-Simple-Cheap-/
-  https://docs.arduino.cc/hacking/software/LibraryTutorial
+  @see https://www.instructables.com/id/USB-Volume-Control-and-Caps-Lock-LED-Simple-Cheap-/
+  @see https://docs.arduino.cc/hacking/software/LibraryTutorial
 
   double press solutin based on:
   MULTI-CLICK:  One Button, Multiple Events Based on "https://jmsarduino.blogspot.com/2009/10/4-way-button-click-double-click-hold.html"
@@ -33,29 +33,30 @@
 
 
 /*
-  //      V0.8 - done
-    use #include <Bounce2.h> for better double  press
-    https://arduino.stackexchange.com/questions/69897/detecting-how-many-button-pressessingle-tap-double-tap-etc-with-two-simultane
+  ///       V0.8 - done
+      use #include <Bounce2.h> for better double  press
+      https://arduino.stackexchange.com/questions/69897/detecting-how-many-button-pressessingle-tap-double-tap-etc-with-two-simultane
 
-    ensure / add support for auto change beween linux and windows.
-    //      V0.8.1 - done
-    * Replaced Bounce2.h as  double  press handler using :
-      MULTI-CLICK:  One Button, Multiple Events Based on "https://jmsarduino.blogspot.com/2009/10/4-way-button-click-double-click-hold.html"
+      ensure / add support for auto change beween linux and windows.
+  
+  ///       V0.8.1 - done
+      * Replaced Bounce2.h as  double  press handler using :
+        MULTI-CLICK:  One Button, Multiple Events Based on "https://jmsarduino.blogspot.com/2009/10/4-way-button-click-double-click-hold.html"
 
-    *  Moved  Button code to lib : Button.h
-    * started on V0.9.0 base classes for loading confs
+      *  Moved  Button code to lib : Button.h
+      * started on V0.9.0 base classes for loading confs
 
-  //        TODO FOR V0.9.0
-    change over to load key map file - maybejson
-    read json AND  read it from pc program or some nice wayy that needs no reflash :
-    https://forum.arduino.cc/t/writing-to-flash-from-application-please-test-and-enjoy/320295
-    https://arduinoplusplus.wordpress.com/2019/04/02/persisting-application-parameters-in-eeprom/
-    https://arduinojson.org/v6/how-to/store-a-json-document-in-eeprom/
-    https://arduinojson.org/
-    https://arduinojson.org/v6/how-to/reduce-memory-usage/
+  ///       TODO FOR V0.9.0
+      change over to load key map file - maybejson
+      read json AND  read it from pc program or some nice wayy that needs no reflash :
+      https://forum.arduino.cc/t/writing-to-flash-from-application-please-test-and-enjoy/320295
+      https://arduinoplusplus.wordpress.com/2019/04/02/persisting-application-parameters-in-eeprom/
+      https://arduinojson.org/v6/how-to/store-a-json-document-in-eeprom/
+      https://arduinojson.org/
+      https://arduinojson.org/v6/how-to/reduce-memory-usage/
 
-  // TODO goals for v1.0.0
-    add serial support between arduione and esp to send keypress or recives keypress
+  ///        TODO goals for v1.0.0
+        add serial support between arduione and esp to send keypress or recives keypress
       - reason this is to add support for esp or raspberry zero with screen that shows media status.
       - if zero is used then a webpage can be used. show and pull data from there. do browser in terminal to save resources, and make it host is own webpages that is displayed
       - if esp is used thne look in to building ui that where some part is static and and only progress bar moves https://forum.arduino.cc/t/tft-touch-reaching-high-refresh-rate-cost-effectively/595823/6
@@ -67,16 +68,16 @@
 */
 
 
-// look up hid projekt to see Languages  or see AlternateLanguageLayout.ino from hid project for example
+/// look up hid projekt to see Languages  or see AlternateLanguageLayout.ino from hid project for example
 #define HID_CUSTOM_LAYOUT  // set this flag to indicate that a custom layout is selected
 #define LAYOUT_DANISH      // set this flag after the above flag to indicate the custom input method is DANISH
 
-// include the HID library
+/// include the HID library
 #include "HID-Project.h"
 #include "FastLED.h"
 #include "Bounce2.h"
 
-// TODO add defin things to struct and load from json
+/// @todo add defin things to struct and load from json
 
 
 
@@ -88,13 +89,14 @@
 #define NUM_LEDS 8
 #define BRIGHTNESS 32
 
-CRGB leds[NUM_LEDS];  // TODO maybe move beacause of loading order of data and info from config
+/// @todo maybe move beacause of loading order of data and info from config
+CRGB leds[NUM_LEDS];
 
 int rgbState = 1;
 int lastrgbState = 0;
 
-// ------------------------------ Buttons press vars ------------------------------
-// press var's for determening a press or double press
+/// ------------------------------ Buttons press vars ------------------------------
+/// press var's for determening a press or double press
 const int holdTime = 1000;
 const unsigned long lastSwitchTime = 0;
 const long DCgap = 250;   // 150 //230   // time for captureing a double press
@@ -112,8 +114,8 @@ const uint8_t buttonPin3Action = MEDIA_NEXT;
 const uint8_t buttonPin4Action1 = KEY_F19;
 const uint8_t buttonPin4Action2 = KEY_F16;
 
-// ------------------------------ DEBUG ------------------------------
-// Determs if Serial.print is enabled
+/// ------------------------------ DEBUG ------------------------------
+/// Determs if Serial.print is enabled
 const bool debugState = false;
 
 void isDebugTruePrintToSerial(String temp) {
@@ -122,8 +124,8 @@ void isDebugTruePrintToSerial(String temp) {
   }
 }
 
-// ------------------------------ Dataconf ------------------------------
-// Struct to hold the general config like colours.
+/// ------------------------------ Dataconf ------------------------------
+/// Struct to hold the general config like colours.
 struct Config {
   char layout;
   uint16_t datapin;
@@ -138,18 +140,18 @@ struct Config {
   uint16_t attemptdelay;
 };
 
-//--------- Internal references ------------
-// (this needs to be below all structs etc..) this may needed to be loaded after struct
-// #include "configLoad.h"
+///--------- Internal references ------------
+/// (this needs to be below all structs etc..) this may needed to be loaded after struct
+/// #include "configLoad.h"
 #include "Button.h"
 
-// -------------- Start Button  ----------------------
-// Bounce2.h lib setup
+/// -------------- Start Button  ----------------------
+/// Bounce2.h lib setup
 Bounce button1Debouncer = Bounce();
 Bounce button2Debouncer = Bounce();
 Bounce button3Debouncer = Bounce();
 
-// Button.h lib setup
+/// Button.h lib setup
 Button Button1(buttonPin1, debugState);
 Button Button2(buttonPin2, debugState);
 Button Button3(buttonPin3, debugState);
@@ -186,48 +188,48 @@ Button Button4(buttonPin4, debugState);
 //------------------ Functions ----------------------------------------------
 
 
-// TODO
-// n = how many leds need be turned on, is used as a array this enables to not turn all the leds.
-// state = used for turning the leds on and of as a toggle
+/// @todo
+/// n = how many leds need be turned on, is used as a array this enables to not turn all the leds.
+/// state = used for turning the leds on and of as a toggle
 void turnOnRGBByState(int cRed, int cGreen, int cBlue, bool state, int ledIndex) {
   if (ledIndex < 0 || ledIndex >= NUM_LEDS) {
-    // Handle out-of-range index
-    return;
+    isDebugTruePrintToSerial("ledIndex out of range");
+    return;  // Handle out-of-range index
   }
 
   switch (state) {
-    case 0:
-      // Turn on LEDs with the specified color
+    case 0:  // Turn on LEDs with the specified color
       FastLED.clear();
       for (int i = 0; i < NUM_LEDS; i++) {
         leds[ledIndex + i] = CRGB(cRed, cGreen, cBlue);
       }
       FastLED.show();
 
-      // Update state variables
+      /// Update state variables
       lastrgbState = 0;
       rgbState = 0;
       break;
 
-    case 1:
-      // Turn off LEDs
+    case 1:  // Turn off LEDs
       FastLED.clear();
       FastLED.show();
 
-      // Update state variables
+      /// Update state variables
       lastrgbState = 0;
       rgbState = 0;
       break;
   }
 }
 
-
-void pressEventHandler(int event) {
+/// Handles the button press events from Button::CheckButtonEvent() where:
+/// Click = 1, Double-Click = 2, Hold = 3, Long Hold = 4
+/// takes two KeyboardKeycode as argument from HID-Project.h for single press and double press for the button that is pressed
+void pressEventHandler(int event, KeyboardKeycode key, KeyboardKeycode key2) {
   switch (event) {
     case 1:
       clickEvent();
       isDebugTruePrintToSerial("single press");
-      Keyboard.write(KEY_F19);
+      Keyboard.write(key);
       switch (rgbState) {
         case 0:
           isDebugTruePrintToSerial("case 0");
@@ -242,7 +244,7 @@ void pressEventHandler(int event) {
       }
       break;
     case 2:
-      doublePress(buttonPin4Action2);
+      doublePress(key2);
       doubleClickEvent();
       break;
     case 3:
@@ -256,29 +258,27 @@ void pressEventHandler(int event) {
   }
 }
 
-
+/// Double press handler, called from pressEventHandler() can be used for other things, takes a KeyboardKeycode as argument from HID-Project.h
 void doublePress(KeyboardKeycode key) {
-  // Double press
   isDebugTruePrintToSerial("double press");
-  // Action to be performed on double press
-  Keyboard.write(key);
+  Keyboard.write(key);  // Action to be performed on double press
   turnOnRGBByState(0, 0, 250, lastrgbState, 0);
 }
 
-// Initializes buttons and debouncers for setup.
+/// Initializes buttons and debouncers for setup.
 void setupButtons() {
-  // Define the pin mode for each pin used
+  /// Define the pin mode for each pin used
   pinMode(buttonPin1, INPUT_PULLUP);
   pinMode(buttonPin2, INPUT_PULLUP);
   pinMode(buttonPin3, INPUT_PULLUP);
   pinMode(buttonPin4, INPUT_PULLUP);
 
-  // BUTTON Setup for Bounce2
+  ///& BUTTON Setup for Bounce2
   button1Debouncer.attach(buttonPin1);
   button2Debouncer.attach(buttonPin2);
   button3Debouncer.attach(buttonPin3);
 
-  // DEBOUNCE INTERVAL IN MILLISECONDS
+  /// DEBOUNCE INTERVAL IN MILLISECONDS
   button1Debouncer.interval(BUTTON_DEBOUNCE_INTERVAL);
   button2Debouncer.interval(BUTTON_DEBOUNCE_INTERVAL);
   button3Debouncer.interval(BUTTON_DEBOUNCE_INTERVAL);
@@ -286,17 +286,20 @@ void setupButtons() {
 
 void setup() {
   Serial.begin(115200);
-  delay(20);  // Delay for recovery
+  /// Delay for recovery
+  delay(20);
 
-  setupButtons();  // encapsulated buttons and debouncers basic setup
+  /// Encapsulated buttons and debouncers basic setup
+  setupButtons();
 
-  // Begin HID connection
+  /// Begin HID connection
   Keyboard.begin();
   Consumer.begin();
 
-  delay(1500);  // 1.5 second delay for recovery and HID connection is sometime unstable at start, this limits it to less.
+  /// 1.5 second delay for recovery and HID connection is sometime unstable at start, this limits it to less. But may not be needed.
+  delay(1500);
 
-  // tell FastLED about the LED strip configuration
+  /// Tell FastLED about the LED strip configuration
   FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS)
     .setCorrection(TypicalLEDStrip)
     .setDither(BRIGHTNESS < 255);
@@ -311,43 +314,44 @@ void setup() {
 }
 
 void loop() {
-  // Calls the Update function as fast as possible.
+  /// Calls the Update function as fast as possible.
   Button1.Update();
   Button2.Update();
   Button3.Update();
   Button4.Update();
 
-  // Button 1 pressed.
+  /// Button 1 pressed.
   if (button1Debouncer.update()) {
     if (button1Debouncer.fell())  // key is pressed down
     {
       Consumer.write(buttonPin1Action);  // send HID command
       isDebugTruePrintToSerial("Back");
-      // Check if button 1 and 2 pressed together
+      
+      /// Check if button 1 and 2 pressed together
       if (digitalRead(buttonPin2) == 0) {
         Button1.IncrementCounter();
       }
     }
   }
 
-  // Button 2 pressed.
+  /// Button 2 pressed.
   if (button2Debouncer.update()) {
     if (button2Debouncer.fell())  // key is pressed down
     {
       Consumer.write(buttonPin2Action);  // send HID command
       isDebugTruePrintToSerial("Play/Pause");
-      // Check if button 1 and 2 pressed together
+      /// Check if button 1 and 2 pressed together
       if (digitalRead(buttonPin1) == 0) {
         Button1.IncrementCounter(Button2);
       }
     }
   }
 
-  // Button 3 pressed.
+  /// Button 3 pressed.
   if (button3Debouncer.update()) {
     if (button3Debouncer.fell())  // key is pressed down
     {
-      // Check if button 3 and 4 pressed together
+      /// Check if button 3 and 4 pressed together
       if (digitalRead(buttonPin4) == 0) {
         Button3.IncrementCounter();
       } else {
@@ -358,11 +362,11 @@ void loop() {
   }
 
   int b = Button4.CheckButtonEvent();
-  pressEventHandler(b);  // this is only for button 4
+  pressEventHandler(b, buttonPin4Action1, buttonPin4Action2);  // this is only for button 4
 }
 
-// =================================================  TODO this part of prep for v0.9.0
-// Events to trigger -- TODO this has to be generict so configlod fits into these events where a button returns event int and then conf can be read an executed
+/// @todo this part of prep for v0.9.0
+/// Events to trigger -- TODO this has to be generict so configlod fits into these events where a button returns event int and then conf can be read an executed
 
 void clickEvent() {
   isDebugTruePrintToSerial("clickEvent");
